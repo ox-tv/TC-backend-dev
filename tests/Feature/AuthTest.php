8636815f1776f63f62e1b967adefa5210ceddf9e
Amir Yousefi <amiryousefi.it@gmail.com>
2020-11-22 18:47:02 +0330

    Update auth test to check for password

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -4,8 +4,9 @@ namespace Tests\Feature;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
@@ -16,13 +17,17 @@ class AuthTest extends TestCase
     public function testRegister()
     {
         $userData = [
-            'email' => $this->faker->safeEmail,
+            'email' => $this->faker->email,
             'password' => $this->faker->password
         ];
 
         $response = $this->json('POST', '/api/register', $userData);
 
         $response->assertStatus(200);
+
+        // unset password to check later
+        $userDataPassword = $userData['password'];
+        unset($userData['password']);
 
         // after registration user must be deactive
         // later that user verifies with a verification code
@@ -35,6 +40,11 @@ class AuthTest extends TestCase
         $userData['status'] = User::STATUS_ACTIVE;
 
         $this->assertDatabaseMissing('users', $userData);
+
+        $user = User::where('email', $userData['email'])->first();
+
+        $this->assertTrue(Hash::check($userDataPassword, $user->password));
+        $this->assertFalse(Hash::check(Str::random(8), $user->password));
 
     }
 }
