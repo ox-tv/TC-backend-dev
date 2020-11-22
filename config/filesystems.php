fdd145b53529c2ec069db33e773e3a46420df7ff
Amir Yousefi <amiryousefi.it@gmail.com>
2020-11-22 13:01:52 +0330

    Add video storage to the filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -65,6 +65,13 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
         ],
 
+        'videos' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/videos'),
+            'url' => env('APP_URL').'/storage/videos',
+            'visibility' => 'public',
+        ],
+
     ],
 
     /*
