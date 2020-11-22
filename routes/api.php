53cc522e4df3aa55c80bd75ade11e33197f084fe
Amir Yousefi <amiryousefi.it@gmail.com>
2020-11-22 17:17:10 +0330

    Remove extra route from api routes

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::apiResource('categories', \App\Http\Controllers\CategoryController::class);
 Route::apiResource('videos', \App\Http\Controllers\VideoController::class);
 
