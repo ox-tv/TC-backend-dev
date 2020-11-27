32390d63d375ed11d6bcc269437580d31a8e33c4
Amir Yousefi <amiryousefi.it@gmail.com>
2020-11-28 03:08:35 +0330

    Add video user relation table migration
    it store video_user relations such as like/dislike

---
+++ database/migrations/2020_11_27_233526_create_video_user_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideoUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('video_user', function (Blueprint $table) {
+            $table->unsignedBigInteger('video_id');
+            $table->unsignedBigInteger('user_id');
+
+            $table->tinyInteger('relation');
+
+            $table->foreign('video_id')
+                ->references('id')
+                ->on('videos');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+
+            $table->primary(['video_id', 'user_id', 'relation']);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('video_user');
+    }
+}
