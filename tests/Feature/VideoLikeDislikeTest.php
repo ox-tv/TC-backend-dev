eb8ffc36be60be383823f6c14c0ef76a46a76732
Amir Yousefi <amiryousefi.it@gmail.com>
2020-11-30 20:16:50 +0330

    Add tests to avoid both like and dislike situation in user video relation

--- tests/Feature/VideoLikeDislikeTest.php
+++ tests/Feature/VideoLikeDislikeTest.php
@@ -111,4 +111,89 @@ class VideoLikeDislikeTest extends TestCase
 
         $response->assertStatus(422);
     }
+
+    public function testCanNotLikedAndDisliked(){
+        // adding a user to auth
+        $user = User::factory()->create();
+        $apiToken = $user->createToken('access_token')->accessToken;
+
+        $video = Video::factory()->create([
+            'status' => Video::STATUS_PUBLISHED,
+        ]);
+
+        $videoId = $video->id;
+
+        $response = $this->json('GET', "/api/videos/{$videoId}/like", [], [
+            'Authorization' => "Bearer {$apiToken}"
+        ]);
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas('user_video', [
+            'video_id' => $videoId,
+            'user_id' => $user->id,
+            'relation' => UserVideo::LIKED_RELATION
+        ]);
+
+        $response = $this->json('GET', "/api/videos/{$videoId}/dislike", [], [
+            'Authorization' => "Bearer {$apiToken}"
+        ]);
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas('user_video', [
+            'video_id' => $videoId,
+            'user_id' => $user->id,
+            'relation' => UserVideo::DISLIKED_RELATION
+        ]);
+
+        $this->assertDatabaseMissing('user_video', [
+            'video_id' => $videoId,
+            'user_id' => $user->id,
+            'relation' => UserVideo::LIKED_RELATION
+        ]);
+    }
+
+    public function testCanNotDislikedAndLiked(){
+        // adding a user to auth
+        $user = User::factory()->create();
+        $apiToken = $user->createToken('access_token')->accessToken;
+
+        $video = Video::factory()->create([
+            'status' => Video::STATUS_PUBLISHED,
+        ]);
+
+        $videoId = $video->id;
+
+        $response = $this->json('GET', "/api/videos/{$videoId}/dislike", [], [
+            'Authorization' => "Bearer {$apiToken}"
+        ]);
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas('user_video', [
+            'video_id' => $videoId,
+            'user_id' => $user->id,
+            'relation' => UserVideo::DISLIKED_RELATION
+        ]);
+
+        $response = $this->json('GET', "/api/videos/{$videoId}/like", [], [
+            'Authorization' => "Bearer {$apiToken}"
+        ]);
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas('user_video', [
+            'video_id' => $videoId,
+            'user_id' => $user->id,
+            'relation' => UserVideo::LIKED_RELATION
+        ]);
+
+        $this->assertDatabaseMissing('user_video', [
+            'video_id' => $videoId,
+            'user_id' => $user->id,
+            'relation' => UserVideo::DISLIKED_RELATION
+        ]);
+    }
+
 }
