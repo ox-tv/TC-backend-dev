ac8c35778a9b2aa236793c9ddafa1ccee2afc5f4
Amir Yousefi <amiryousefi.it@gmail.com>
2020-11-30 19:58:51 +0330

    Add video like/dislike initial tests

--- tests/Feature/VideoLikeDislikeTest.php
+++ tests/Feature/VideoLikeDislikeTest.php
@@ -38,4 +38,77 @@ class VideoLikeDislikeTest extends TestCase
             'relation' => VideoUser::LIKED_RELATION
         ]);
     }
+
+    public function testVideoCanBeDisliked()
+    {
+        // adding a user to auth
+        $user = User::factory()->create();
+        $apiToken = $user->createToken('access_token')->accessToken;
+
+        $video = Video::factory()->create([
+            'status' => Video::STATUS_PUBLISHED,
+        ]);
+
+        $videoId = $video->id;
+
+        $response = $this->json('GET', "/api/videos/{$videoId}/dislike", [], [
+            'Authorization' => "Bearer {$apiToken}"
+        ]);
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas('user_video', [
+            'video_id' => $videoId,
+            'user_id' => $user->id,
+            'relation' => VideoUser::DISLIKED_RELATION
+        ]);
+    }
+
+    public function testVideoCanBeLikedOnlyOnce(){
+        // adding a user to auth
+        $user = User::factory()->create();
+        $apiToken = $user->createToken('access_token')->accessToken;
+
+        $video = Video::factory()->create([
+            'status' => Video::STATUS_PUBLISHED,
+        ]);
+
+        $videoId = $video->id;
+
+        $response = $this->json('GET', "/api/videos/{$videoId}/like", [], [
+            'Authorization' => "Bearer {$apiToken}"
+        ]);
+
+        $response->assertStatus(200);
+
+        $response = $this->json('GET', "/api/videos/{$videoId}/like", [], [
+            'Authorization' => "Bearer {$apiToken}"
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    public function testVideoCanBeDislikedOnlyOnce(){
+        // adding a user to auth
+        $user = User::factory()->create();
+        $apiToken = $user->createToken('access_token')->accessToken;
+
+        $video = Video::factory()->create([
+            'status' => Video::STATUS_PUBLISHED,
+        ]);
+
+        $videoId = $video->id;
+
+        $response = $this->json('GET', "/api/videos/{$videoId}/dislike", [], [
+            'Authorization' => "Bearer {$apiToken}"
+        ]);
+
+        $response->assertStatus(200);
+
+        $response = $this->json('GET', "/api/videos/{$videoId}/dislike", [], [
+            'Authorization' => "Bearer {$apiToken}"
+        ]);
+
+        $response->assertStatus(422);
+    }
 }
