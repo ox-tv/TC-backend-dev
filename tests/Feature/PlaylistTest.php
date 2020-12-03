ff18e20c714cf5c13129bb48859ad981ea4dbd5d
Amir Yousefi <amiryousefi.it@gmail.com>
2020-12-03 10:14:27 +0330

    Add playlist CRUD tests

---
+++ tests/Feature/PlaylistTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Playlist;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PlaylistTest extends TestCase
+{
+    use DatabaseTransactions, WithFaker;
+
+    public function testPlaylistIndex()
+    {
+        // adding a user to auth
+        $user = User::factory()->create();
+        $apiToken = $user->createToken('access_token')->accessToken;
+
+        $response = $this->json('GET', '/api/playlist', [], [
+            'Authorization' => "Bearer {$apiToken}"
+        ]);
+
+        $response->assertStatus(200);
+
+        $response->assertJson([]);
+    }
+
+    public function testPlaylistStore(){
+        // adding a user to auth
+        $user = User::factory()->create();
+        $apiToken = $user->createToken('access_token')->accessToken;
+
+        $playlistData = [
+            'name' => $this->faker->text(50)
+        ];
+
+        $response = $this->json('POST', '/api/playlist', $playlistData, [
+            'Authorization' => "Bearer {$apiToken}"
+        ]);
+
+        $response->assertStatus(201);
+
+        $this->assertDatabaseHas('playlists', $playlistData);
+    }
+
+    public function testPlaylistUpdate(){
+        // adding a user to auth
+        $user = User::factory()->create();
+        $apiToken = $user->createToken('access_token')->accessToken;
+
+        $playlist = Playlist::factory()->create();
+
+        $playlistUpdatedData = [
+            'name' => $this->faker->text(50)
+        ];
+
+        $response = $this->json('PUT', "/api/playlist/{$playlist->id}", $playlistUpdatedData, [
+            'Authorization' => "Bearer {$apiToken}"
+        ]);
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas('playlists', $playlistUpdatedData);
+
+        $playlist->refresh();
+
+        $this->assertEquals($playlist->name, $playlistUpdatedData['name']);
+
+    }
+
+    public function testPlaylistDelete(){
+        // adding a user to auth
+        $user = User::factory()->create();
+        $apiToken = $user->createToken('access_token')->accessToken;
+
+        $playlist = Playlist::factory()->create();
+
+        $response = $this->json('DELETE', "/api/playlist/{$playlist->id}", [], [
+            'Authorization' => "Bearer {$apiToken}"
+        ]);
+
+        $response->assertStatus(200);
+
+        $this->assertDeleted('playlists', $playlist->toArray());
+
+    }
+}
