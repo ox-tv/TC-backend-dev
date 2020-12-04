1dff23475a2c7194dc150845379d1ff63404336a
Amir Yousefi <amiryousefi.it@gmail.com>
2020-12-04 19:25:54 +0330

    Add tests to channel so it checks if the owner of the channel can only add/remove video

--- tests/Feature/ChannelVideoTest.php
+++ tests/Feature/ChannelVideoTest.php
@@ -19,7 +19,9 @@ class ChannelVideoTest extends TestCase
         $user = User::factory()->create();
         $apiToken = $user->createToken('access_token')->accessToken;
 
-        $channel = Channel::factory()->create();
+        $channel = Channel::factory()->create([
+            'user_id' => $user->id
+        ]);
         $video = Video::factory()->create();
 
         $response = $this->json('PUT', "/api/channels/{$channel->id}/add/{$video->id}", [], [
@@ -39,7 +41,9 @@ class ChannelVideoTest extends TestCase
         $user = User::factory()->create();
         $apiToken = $user->createToken('access_token')->accessToken;
 
-        $channel = Channel::factory()->create();
+        $channel = Channel::factory()->create([
+            'user_id' => $user->id
+        ]);
         $video = Video::factory()->create();
 
         $channel->videos()->attach($video);
@@ -54,6 +58,60 @@ class ChannelVideoTest extends TestCase
             'channel_id' => $channel->id,
             'video_id' => $video->id
         ]);
+    }
+
+    public function testVideoCanOnlyBeAddedToOwnedChannel(){
+        // adding a user to auth
+        $user = User::factory()->create();
+        $apiToken = $user->createToken('access_token')->accessToken;
+
+        $anotherUser = User::factory()->create();
+
+        $video = Video::factory()->create();
+
+        $channel = Channel::factory()->create([
+            'user_id' => $anotherUser->id
+        ]);
+
+        $response = $this->json('PUT', "/api/channels/{$channel->id}/add/{$video->id}", [], [
+            'Authorization' => "Bearer {$apiToken}"
+        ]);
+
+        $response->assertStatus(404);
+
+        $this->assertDatabaseMissing('channel_video', [
+            'channel_id' => $channel->id,
+            'video_id' => $video->id
+        ]);
+
+    }
+
+    public function testVideoCanOnlyBeRemovedFromOwnedChannel(){
+        // adding a user to auth
+        $user = User::factory()->create();
+        $apiToken = $user->createToken('access_token')->accessToken;
+
+        $anotherUser = User::factory()->create();
+
+        $video = Video::factory()->create();
+
+        $channel = Channel::factory()->create([
+            'user_id' => $anotherUser->id
+        ]);
+
+        $channel->videos()->attach($video);
+
+        $response = $this->json('PUT', "/api/channels/{$channel->id}/remove/{$video->id}", [], [
+            'Authorization' => "Bearer {$apiToken}"
+        ]);
+
+        $response->assertStatus(404);
+
+        $this->assertDatabaseHas('channel_video', [
+            'channel_id' => $channel->id,
+            'video_id' => $video->id
+        ]);
+
     }
 
 
