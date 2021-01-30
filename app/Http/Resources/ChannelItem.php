2e67a6dc8c82ad5bb19b100e443a4c53303ce56f
Amir <a.mir.yousefi.it@gmail.com>
2021-01-30 14:26:39 +0330

    Update channel resource to add extra fields

--- app/Http/Resources/ChannelItem.php
+++ app/Http/Resources/ChannelItem.php
@@ -17,9 +17,20 @@ class ChannelItem extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'description' => $this->description,
             'slug' => $this->slug,
             'url_hash' => $this->url_hash,
-            'avatar' => $this->avatar ?? asset('assets/images/default-avatar.png')
+            'cover' => $this->cover ?? asset('assets/images/default-video-thumbnail.png'),
+            'avatar' => $this->avatar ?? asset('assets/images/default-avatar.png'),
+            "instagram" => $this->instagram,
+            "facebook" => $this->facebook,
+            "twitter" => $this->twitter,
+            "website" => $this->website,
+            "slogan" => $this->slogan,
+            "user" => $this->owner,
+            "status" => $this->status,
+            "created_at" => $this->created_at,
+            "updated_at" => $this->updated_at,
         ];
     }
 }
