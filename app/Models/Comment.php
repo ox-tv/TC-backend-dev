fa9113f937425cae12cf1b2d8ccd76b34a3fe281
Amir <a.mir.yousefi.it@gmail.com>
2021-02-07 17:53:56 +0330

    Add order desc to comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\OrderDescScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -10,6 +11,11 @@ class Comment extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    protected static function booted()
+    {
+        static::addGlobalScope(new OrderDescScope);
+    }
 
     // Relations
 
