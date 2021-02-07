14315ba26d6726fb10317bb326276e0fe3e73b6b
Amir <a.mir.yousefi.it@gmail.com>
2021-02-07 15:43:41 +0330

    Update comment relation

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -14,7 +14,11 @@ class Comment extends Model
     // Relations
 
     public function video(){
-        return $this->belongsTo('App\Models\Comment');
+        return $this->belongsTo('App\Models\Video');
+    }
+
+    public function user(){
+        return $this->belongsTo('App\Models\User');
     }
 
     public function parent(){
@@ -27,5 +31,9 @@ class Comment extends Model
 
     public function dislikedBy(){
         return $this->belongsToMany('App\Models\User')->withPivot('relation')->where('relation', CommentUser::DISLIKED_RELATION);
+    }
+
+    public function replies(){
+        return $this->hasMany('App\Models\Comment', 'parent_id', 'id');
     }
 }
