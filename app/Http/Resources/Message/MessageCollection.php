90ca29f96cf99a07a59ca0e00a3659cd0c0615b7
Amir <a.mir.yousefi.it@gmail.com>
2021-03-03 13:36:28 +0330

    Add message resource collections

---
+++ app/Http/Resources/Message/MessageCollection.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\Message;
+
+use App\Http\Resources\MessageItem;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class MessageCollection extends ResourceCollection
+{
+
+    public $collects = MessageItem::class;
+
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Support\Collection
+     */
+    public function toArray($request)
+    {
+        return $this->collection;
+    }
+}
---
+++ app/Http/Resources/Message/MessageItem.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\Message;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MessageItem extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'subject' => $this->subject,
+            'message' => $this->message,
+        ];
+    }
+}
