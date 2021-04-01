8db239b0e467ed4b429f868b57ed428fd1d94e20
Amir <a.mir.yousefi.it@gmail.com>
2021-04-01 12:41:21 +0430

    Add publisher register request

---
+++ app/Http/Requests/PublisherRegister.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class PublisherRegister extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => [
+                'required', 'string', 'email', 'max:255',
+                function($attribute, $value, $fail){
+                    // check if user is deleted
+                    $user = User::where('email', $value)->first();
+                    if ($user && $user->deleted_at) {
+                        $fail(__('users.validation.account_deleted'));
+                    }
+                },
+                Rule::unique('users', 'email')->whereNotNull('email_verified_at')],
+            'password' => ['required', 'string', 'min:8'],
+            'youtube_url' => 'required',
+            'verification_url' => 'required'
+        ];
+    }
+
+}
