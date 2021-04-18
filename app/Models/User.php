9a9113d45a603447c82ea2cf536045d4e2f8b951
Amir <a.mir.yousefi.it@gmail.com>
2021-04-18 11:03:00 +0430

    Add relation comment to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -85,6 +85,7 @@ class User extends Authenticatable
         return $query;
     }
 
+    // Relations
 
     public function channel(){
         return $this->hasOne('App\Models\Channel', 'user_id');
