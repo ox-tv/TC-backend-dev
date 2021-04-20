2a44da17bcb8bb6b1f06f028de9c8834ec5f8a99
Amir <a.mir.yousefi.it@gmail.com>
2021-04-20 17:08:30 +0430

    Add message scopes

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -10,6 +10,20 @@ class Message extends Model
 {
     use HasFactory, SoftDeletes;
 
+    // search scopes
+
+    public function scopeMine($query){
+        if(auth('api')->check()){
+            $query->where('user_id', auth('api')->user()->id);
+        }
+        return $query;
+    }
+
+    public function scopeDepartment($query, $departmentId){
+        $query->where('department_id', $departmentId);
+        return $query;
+    }
+
     public function user(){
         return $this->belongsTo('App\Models\User');
     }
