b5fb549bf169075827dd4d2071d4349d0736a90c
Amir <a.mir.yousefi.it@gmail.com>
2021-04-21 06:07:31 +0430

    Fix video store validation error

--- app/Http/Requests/VideoStore.php
+++ app/Http/Requests/VideoStore.php
@@ -27,7 +27,6 @@ class VideoStore extends FormRequest
     public function rules()
     {
         return [
-            'categories.*.id' => 'exists:categories,id',
             'category' => 'exists:categories,id',
             'status' => Rule::in([Video::STATUS_TEXT[Video::STATUS_DRAFT], Video::STATUS_TEXT[Video::STATUS_PUBLISHED]]),
             'youtube_link' => 'url'
