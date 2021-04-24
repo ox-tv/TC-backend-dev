334217eaddf917d89e081112fb81b6d07b5fbd18
Amir <a.mir.yousefi.it@gmail.com>
2021-04-24 17:30:06 +0430

    Update channel summary resource

--- app/Http/Resources/ChannelSummaryItem.php
+++ app/Http/Resources/ChannelSummaryItem.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Channel;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ChannelSummaryItem extends JsonResource
@@ -20,16 +21,21 @@ class ChannelSummaryItem extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'slug' => $this->slug,
-            'points' => $this->points,
             'subscribers_count' => $this->subscribers->count(),
             'uploads_count' => $this->uploads_count,
             'total_views' => $this->total_views,
             'total_likes' => $this->total_likes,
+            'total_dislikes' => $this->total_dislikes,
+            'comments_count' => $this->total_comments,
+            'points' => $this->points,
+            // TODO:: calculate real watch hours
+            'watch_hours' => rand(1,99),
             'hero_subscribers_count' => $this->heroSubscribers->count(),
             "owner" => $this->when($withOwner, UserItem::make($this->owner)),
             'url_hash' => $this->url_hash,
             'avatar' => $this->avatar,
             "slogan" => $this->slogan,
+            "status" => $this->status ? Channel::STATUS_TEXT[$this->status] : null,
             'is_subscribed' => auth('api')->check() ? ($this->subscribers()->find(auth('api')->user()->id) ? true : false) : false,
         ];
     }
