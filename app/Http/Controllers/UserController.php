6db3fe13e6f300d71c30268645b953794a4a8081
aahelali <aahelali@gmail.com>
2021-04-24 15:47:41 +0430

    delete user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -120,9 +120,13 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+
+        return response()->json([
+            'message' => 'general.successful'
+        ]);
     }
 
     /**
