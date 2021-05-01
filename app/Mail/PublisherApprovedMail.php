e8b39d10538d79cc01b0696a6f93d281a01609e8
Ali <aahelali@gmail.com>
2021-05-01 15:19:11 +0430

    Add mail to publisher with custom email template

---
+++ app/Mail/PublisherApprovedMail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class PublisherApprovedMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $app_name = config("app.name");
+
+        return $this->subject( "{$app_name} - Publisher request approved")->view('emails.publisher-approved');
+    }
+}
