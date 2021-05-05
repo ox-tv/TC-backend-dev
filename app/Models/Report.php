a7767fc7e86bb4f2c2ffbd8f70957609db53a331
Ali <aahelali@gmail.com>
2021-05-05 20:34:42 +0430

    Add reports mdoel with relations and scopes

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -7,10 +7,34 @@ use Illuminate\Database\Eloquent\Model;
 class Report extends Model
 {
 
+    // Scopes
+    public function scopeVideo($query){
+        $query->where('reportable_type', Video::class);
+        return $query;
+    }
+
+    public function scopeChannel($query){
+        $query->where('reportable_type', Channel::class);
+        return $query;
+    }
+
+    public function scopeComment($query){
+        $query->where('reportable_type', Comment::class);
+        return $query;
+    }
+
     // Relations
 
     public function reportable()
     {
         return $this->morphTo();
+    }
+
+    public function user(){
+        return $this->belongsTo('App\Models\User', 'user_id');
+    }
+
+    public function reported_user(){
+        return $this->belongsTo('App\Models\User', 'reported_user_id');
     }
 }
