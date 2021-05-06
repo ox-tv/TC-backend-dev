7920120f76bf2c90d76730865b0e5103af256e6c
Amir <a.mir.yousefi.it@gmail.com>
2021-05-07 01:23:13 +0430

    Update publisher application message

--- resources/lang/en/publisher.php
+++ resources/lang/en/publisher.php
@@ -3,7 +3,7 @@
 return [
 
     'application_subject' => 'Request for becoming a publisher',
-    'application_message' => 'Email: :email \n YouTube Channel: :youtube_url \n Verification URL: :verification_url',
+    'application_message' => 'Email: :email \n YouTube channel name: :channel_name \n YouTube channel URL: :youtube_url \n Verification URL: :verification_url',
 
     'messages' => [
         'wait_for_verification' => 'Your request submitted, TodaysCrypto support will contact you as soon as we can verify your request'
