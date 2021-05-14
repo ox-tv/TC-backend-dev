5f2ad7010ee477b1078939c8e48c824f941c642e
Ali <aahelali@gmail.com>
2021-05-14 17:48:08 +0430

    Add email class and template

---
+++ app/Mail/PasswordResetMail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class PasswordResetMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $link;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($link)
+    {
+        $this->link = $link;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $app_name = config("general.SITE_NAME");
+
+        return $this->subject( "{$app_name} - Password Reset")->view('emails.password-reset');
+    }
+}
