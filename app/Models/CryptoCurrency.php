2c04e34bcec463800ad0f86ddc61b30a900a8ca8
Ali <aahelali@gmail.com>
2021-05-17 19:31:24 +0430

    Add search scopes to model

--- app/Models/CryptoCurrency.php
+++ app/Models/CryptoCurrency.php
@@ -7,5 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class CryptoCurrency extends Model
 {
 
+    public function scopeSearchName($query, $keyword){
+        $query->where('name', 'LIKE', '%'.$keyword.'%');
+        return $query;
+    }
+
+    public function scopeSearchSymbol($query, $keyword){
+        $query->where('symbol', 'LIKE', '%'.$keyword.'%');
+        return $query;
+    }
 
 }
