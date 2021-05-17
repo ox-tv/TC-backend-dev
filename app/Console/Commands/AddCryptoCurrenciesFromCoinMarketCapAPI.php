58fb5c694a8db645110ba853f2e5eaed430bb140
Ali <aahelali@gmail.com>
2021-05-17 14:16:58 +0430

    Add command for get all cryptoCurencies from coin market cap and insert to database

---
+++ app/Console/Commands/AddCryptoCurrenciesFromCoinMarketCapAPI.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Libraries\CoinMarketCapClient;
+use App\Models\CryptoCurrency;
+use Illuminate\Console\Command;
+
+class AddCryptoCurrenciesFromCoinMarketCapAPI extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'crypto_currencies:add';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'add crypto currencies from CoinMarketCap API';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $client = new CoinMarketCapClient();
+
+        $start = 1;
+        $limit = 1000;
+
+        do{
+            $response = $client->GetCryptoCurrencies($start, $limit);
+
+            if(empty($response['data'])){
+                break;
+            }
+
+            $data = [];
+            foreach ($response['data'] as $value){
+                $data[] = [
+                    'symbol' => $value['symbol'],
+                    'name' => $value['name'],
+                    'slug' => $value['slug'],
+                ];
+            }
+
+            CryptoCurrency::upsert($data, ['slug'], ['name', 'symbol']);
+
+            $start += $limit;
+
+        } while(1);
+
+        return 0;
+    }
+}
