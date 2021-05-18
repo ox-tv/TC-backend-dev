ec6e4ba0c41d32fd78f40e8636c69213f3599bfa
Ali <aahelali@gmail.com>
2021-05-18 19:33:11 +0430

    Add crypto currencies to form requests

--- app/Http/Requests/VideoStore.php
+++ app/Http/Requests/VideoStore.php
@@ -28,6 +28,7 @@ class VideoStore extends FormRequest
     {
         return [
             'categories.*.id' => 'exists:categories,id',
+            'crypto_currencies.*' => 'exists:crypto_currencies,id',
             'category' => 'exists:categories,id',
             'status' => Rule::in([Video::STATUS_TEXT[Video::STATUS_DRAFT], Video::STATUS_TEXT[Video::STATUS_PUBLISHED]]),
             'youtube_link' => 'url',
