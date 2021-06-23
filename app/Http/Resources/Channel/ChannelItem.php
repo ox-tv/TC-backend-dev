d8d2d92f20a390f0fcdf64ab3530ed4ec4a9be50
Ali <aahelali@gmail.com>
2021-06-23 11:15:43 +0430

    Add ChannelItem

---
+++ app/Http/Resources/Channel/ChannelItem.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Resources\Channel;
+
+use App\Http\Resources\User\UserMinimalItem;
+use App\Models\Channel;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ChannelItem extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        $include = explode(',', $request->get('include', ''));
+
+        $withOwner = in_array('owner', $include) || $this->relationLoaded('owner');
+
+        $owner = ($withOwner)? UserMinimalItem::make($this->owner) : [];
+
+
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'slug' => $this->slug,
+            'subscribers_count' => $this->subscribers->count(),
+            'uploads_count' => $this->uploads_count,
+            'total_views' => $this->total_views,
+            'total_likes' => $this->total_likes,
+            'total_dislikes' => $this->total_dislikes,
+            'comments_count' => $this->total_comments,
+            'points' => $this->points,
+            'watch_time' => $this->videos()->sum("watch_time"),
+            'hero_subscribers_count' => $this->heroSubscribers->count(),
+            'url_hash' => $this->url_hash,
+            'cover' => $this->cover,
+            'avatar' => $this->avatar,
+            "instagram" => $this->instagram,
+            "facebook" => $this->facebook,
+            "twitter" => $this->twitter,
+            "website" => $this->website,
+            "slogan" => $this->slogan,
+            "owner" => $this->when($withOwner, $owner),
+            "status" => $this->status ? Channel::STATUS_TEXT[$this->status] : null,
+            "import_request_status" => Channel::IMPORT_STATUS_TEXT[$this->import_request_status]?? null,
+            'is_subscribed' => auth('api')->check() ? ($this->subscribers()->find(auth('api')->user()->id) ? true : false) : false,
+            "created_at" => $this->created_at,
+            "updated_at" => $this->updated_at,
+        ];
+    }
+}
