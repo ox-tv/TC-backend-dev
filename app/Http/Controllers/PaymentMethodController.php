8e25287a6a0cc3483193328d5fe9b5efe87c6652
Ali <aahelali@gmail.com>
2021-07-11 14:59:40 +0430

    Add getting all payment methods

---
+++ app/Http/Controllers/PaymentMethodController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PaymentMethod;
+use Illuminate\Http\Request;
+
+class PaymentMethodController extends Controller
+{
+    public function index()
+    {
+        return PaymentMethod::all();
+    }
+}
