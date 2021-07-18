c84e66016c371e176afb373b16170285231f3fd4
Ali <aahelali@gmail.com>
2021-07-18 16:50:37 +0430

    Add TCNotification and NotificationREpository

---
+++ app/Repository/Eloquent/NotificationRepository.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\Repository\Eloquent;
+
+
+use App\Models\Notification;
+use App\Repository\NotificationRepositoryInterface;
+use Illuminate\Support\Facades\DB;
+
+class NotificationRepository implements NotificationRepositoryInterface
+{
+
+    public function store($users, $type, $scope, $userGroup, $payload = null, $entityType = null, $entityId = null, $from = null)
+    {
+        $notification = new Notification();
+        $notification->type = $type;
+        $notification->scope = array_flip(Notification::SCOPE_TEXT)[$scope];
+        $notification->payload = $payload;
+        $notification->user_group = array_flip(Notification::USER_GROUP_TEXT)[$userGroup];
+        $notification->sender_id = $from;
+        $notification->entity_type = $entityType;
+        $notification->entity_id = $entityId;
+
+        DB::transaction(function () use ($notification, $users){
+            $notification->save();
+            $notification->users()->attach($users->pluck('id')->toArray());
+        });
+
+        return true;
+    }
+}
