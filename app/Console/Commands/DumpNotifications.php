dc3aca626bf118dc6c322d931c393787ee4c4b81
Ali <aahelali@gmail.com>
2021-07-20 19:53:06 +0430

    Refactor DumpNotifications

--- app/Console/Commands/DumpNotifications.php
+++ app/Console/Commands/DumpNotifications.php
@@ -28,16 +28,6 @@ class DumpNotifications extends Command
     protected $description = 'Dump notifications table and pivot table';
 
     /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    /**
      * Execute the console command.
      *
      * @return int
