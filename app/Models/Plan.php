56087afd0e6123f0dacfa6998566b0df05c0bf30
Ali <aahelali@gmail.com>
2021-07-25 11:05:07 +0430

    Add softDelete to Pricing and Plans

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Plan extends Model
 {
+    use SoftDeletes;
+
     const STATUS_INACTIVE = 1;
     const STATUS_ACTIVE = 2;
 
