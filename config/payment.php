b89f4364618b6e1a4f52acbe75726725b30bda70
Ali <aahelali@gmail.com>
2021-09-20 14:10:56 +0430

    Set Coinbase redirect and cancel url

--- config/payment.php
+++ config/payment.php
@@ -6,6 +6,8 @@ return [
         'webhook_secret' => env('COINBASE_WEBHOOK_SECRET'),
         'base_url' => env('COINBASE_BASE_URL', 'https://api.commerce.coinbase.com'),
         'api_version' => env('COINBASE_API_VERSION', '2018-03-22'),
+        'redirect_url' => env('COINBASE_REDIRECT_URL', ''),
+        'cancel_url' => env('COINBASE_CANCEL_URL', ''),
     ],
 
 
