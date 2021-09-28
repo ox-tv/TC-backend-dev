ea5bbd5e22a2bb854d2a7ba20d73cddbe7a24700
Ali <aahelali@gmail.com>
2021-09-28 12:36:25 +0330

    Add cashier settings

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,11 +8,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
 use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, SoftDeletes, HasRoles;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes, HasRoles, Billable;
 
 
     const STATUS_INACTIVE = 1;
