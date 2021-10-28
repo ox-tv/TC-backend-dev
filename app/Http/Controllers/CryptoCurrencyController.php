c2e6a2370b19998baf72b5a37250406bef0ac2a2
Ali <aahelali@gmail.com>
2021-10-28 17:50:11 +0330

    Refactor cryptoCurrencies

--- app/Http/Controllers/CryptoCurrencyController.php
+++ app/Http/Controllers/CryptoCurrencyController.php
@@ -6,6 +6,7 @@ use App\Http\Resources\CryptoCurrency\CryptoCurrencyItem;
 use App\Libraries\CoinMarketCapClient;
 use App\Models\Category;
 use App\Models\CryptoCurrency;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 
@@ -40,47 +41,31 @@ class CryptoCurrencyController extends Controller
             $data = $query->paginate();
         }
 
-        $ratios = $this->GetRatios($data);
-
+        $need_to_get_ratio = [];
         foreach($data as $crypto_currency){
-            $crypto_currency->ratio = $ratios[$crypto_currency->id];
+            if(empty($crypto_currency->prices) || $crypto_currency->updated_at < Carbon::now()->subMinutes(10)){
+                $need_to_get_ratio[] = $crypto_currency;
+            }
         }
+        $this->GetRatios($need_to_get_ratio);
 
         return CryptoCurrencyItem::collection($data);
     }
 
     private function GetRatios($crypto_currencies)
     {
-        $result = [];
-        $need_to_get = [];
+        $client = new CoinMarketCapClient();
+
+        $slugs = array_column($crypto_currencies, 'slug');
+
+        $res = $client->GetPriceRatio(implode(',', $slugs))?? abort(404);
 
         foreach($crypto_currencies as $crypto_currency){
-            if(cache()->has("crypto_currency_{$crypto_currency->id}_USD")){
-                $result[$crypto_currency->id] = cache()->get("crypto_currency_{$crypto_currency->id}_USD");
-            }else{
-                $need_to_get[] = $crypto_currency;
-            }
+            $crypto_currency->prices = $res[$crypto_currency->slug]??  null;
+            $crypto_currency->save();
         }
 
-        if(!empty($need_to_get)){
-            $client = new CoinMarketCapClient();
-
-            $slugs = array_column($need_to_get, 'slug');
-
-            $res = $client->GetPriceRatio(implode(',', $slugs))?? abort(404);
-
-            foreach($need_to_get as $crypto_currency){
-                $result[$crypto_currency->id] = cache()->remember(
-                    "crypto_currency_{$crypto_currency->id}_USD",
-                    60 * 5,
-                    function () use ($crypto_currency, $res) {
-                        return $res[$crypto_currency->slug]??  null;
-                    }
-                );
-            }
-        }
-
-        return $result;
+        return true;
     }
 
     public function favorites()
