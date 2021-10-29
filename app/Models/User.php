cfa0ebf0b17f875c78c9c5d961cc1e58d84d6384
Ali <aahelali@gmail.com>
2021-10-29 18:12:38 +0330

    Add statistics relation into user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -187,6 +187,10 @@ class User extends Authenticatable
         return $this->belongsToMany('App\Models\Pricing')->withTimestamps();
     }
 
+    public function statistics(){
+        return $this->hasMany('App\Models\UserStatisticsDaily');
+    }
+
 
     // Attributes
     public function getIsHeroAttribute(){
