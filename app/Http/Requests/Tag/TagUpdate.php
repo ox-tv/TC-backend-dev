b518535c0cdd29d573523bbcb721d1743f06d8c8
Ali <aahelali@gmail.com>
2021-11-18 14:27:41 +0330

    Add TagStore and TagUpdate FormRequest

---
+++ app/Http/Requests/Tag/TagUpdate.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\Tag;
+
+use App\Models\Chapter;
+use App\Models\Message;
+use App\Models\MessageUser;
+use App\Models\Tag;
+use App\Models\Video;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class TagUpdate extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $tag_id = $this->route('tag');
+
+        return [
+            'name' => ['required', Rule::unique('tags', 'name')->ignore($tag_id)],
+            'status' => ['nullable', Rule::in(Tag::STATUS_TEXT)],
+        ];
+    }
+}
