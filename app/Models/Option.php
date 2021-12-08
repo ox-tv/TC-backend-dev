60c6a880aaeef2e4d2501975f7a1c2ea73393585
Ali <aahelali@gmail.com>
2021-12-08 16:23:10 +0330

    Define option keys

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Option extends Model
 {
     use HasFactory;
+
+    // Define Keys
+    const KEY_REPORT_VIDEO_REASONS = 'report_video_reasons';
+    const KEY_REPORT_COMMENT_REASONS = 'report_comment_reasons';
+    const KEY_HIDE_VIDEO_REASONS = 'video_hide_reasons';
+    const KEY_DELETE_VIDEO_REASONS = 'video_delete_reasons';
+    const KEY_DELETE_COMMENT_REASONS = 'comment_delete_reasons';
+    const KEY_REJECT_PUBLISHER_REQUEST_REASONS = 'publisher_request_reject_reasons';
+
 }
