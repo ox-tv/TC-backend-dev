6f8c7c4ea662bf3c33966a0bac583bb3602ddeda
Ali <aahelali@gmail.com>
2021-12-16 13:47:12 +0330

    Add becomeAPublisher to publisherController and refactor it

--- resources/lang/en/publisher.php
+++ resources/lang/en/publisher.php
@@ -7,7 +7,7 @@ return [
     'application_reply_for_youtube_platform_users' => 'As you are active on YouTube already we need some information to verify you. \n Please provide your YouTube channel url. Also you need to share a private YouTube video with the title (Today’s Crypto) to verify@todayscrypto.com and paste your video link here. \n We will go though your verification process and let you know once it’s done.',
 
     'messages' => [
-        'wait_for_verification' => 'Your request submitted, TodaysCrypto support will contact you as soon as we can verify your request'
+        'wait_for_verification' => 'Your request submitted, TodaysCrypto support will contact you as soon as we can verify your request',
     ]
 
 ];
