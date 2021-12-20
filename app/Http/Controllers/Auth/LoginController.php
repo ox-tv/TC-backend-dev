cd412ed503b96ebf819ec14f450f0a2033e93e88
Ali <aahelali@gmail.com>
2021-12-20 13:16:57 +0330

    Move checking inactive account to method

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
         $login = $request->get('email')?:$request->get('login');
         $loginType = filter_var($login, FILTER_VALIDATE_EMAIL)? 'email': 'username';
 
-        if(Auth::validate([$loginType => $login, 'password' => $this->request->get('password')])){
+        if(Auth::validate([$loginType => $login, 'password' => $request->get('password')])){
             $user = Auth::getLastAttempted();
             if($user->status == User::STATUS_INACTIVE) {
                 return response()->json(['code'=>401, 'message'=>__('auth.inactive_account')], 401);
