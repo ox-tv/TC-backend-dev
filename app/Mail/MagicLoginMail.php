8e4a0e7c22836291354c4935c64a41165f85162c
Ali <aahelali@gmail.com>
2022-01-01 19:50:29 +0330

    Add magic login mail class and template

---
+++ app/Mail/MagicLoginMail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class MagicLoginMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $verification_link;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($link)
+    {
+        $this->verification_link = $link;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $app_name = config("general.SITE_NAME");
+
+        return $this->subject( "{$app_name} - Magic Login")->view('emails.magic-login-dark');
+    }
+}
