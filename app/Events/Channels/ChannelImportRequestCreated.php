aa30809381fe8da9cef80e0ba3d394af2d1be78c
Ali <aahelali@gmail.com>
2022-01-18 14:58:24 +0330

    Add ChannelImportRequestCreated event and listener

---
+++ app/Events/Channels/ChannelImportRequestCreated.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Events\Channels;
+
+use App\Models\Message;
+use App\Models\User;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ChannelImportRequestCreated
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $user;
+    public $message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($user, Message $message)
+    {
+        $this->user = $user;
+        $this->message = $message;
+    }
+
+}
