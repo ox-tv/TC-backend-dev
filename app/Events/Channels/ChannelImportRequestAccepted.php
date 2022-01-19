27195d4d9db3c509f0dfbac413f336b6cbf77db0
Ali <aahelali@gmail.com>
2022-01-19 15:12:38 +0330

    Add ChannelImportRequestAccepted event and listener

---
+++ app/Events/Channels/ChannelImportRequestAccepted.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Events\Channels;
+
+use App\Models\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ChannelImportRequestAccepted
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $channel;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Channel $channel)
+    {
+        $this->channel = $channel;
+    }
+
+}
