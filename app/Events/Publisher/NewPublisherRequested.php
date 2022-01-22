37645be36429015ce97277b19f4af87c23bc65a9
Ali <aahelali@gmail.com>
2022-01-22 14:01:11 +0330

    Add NewPublisherRequested event and listener

---
+++ app/Events/Publisher/NewPublisherRequested.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Events\Publisher;
+
+use App\Models\Message;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class NewPublisherRequested
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $user;
+    public $message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($user, Message $message)
+    {
+        $this->user = $user;
+        $this->message = $message;
+    }
+
+}
