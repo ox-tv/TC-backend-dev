def406c2037ffbe840b6bc680c30b4cf45a6c79b
Ali <aahelali@gmail.com>
2022-01-29 17:29:00 +0330

    Refactor upload routes

--- routes/partial-routes/upload.php
+++ routes/partial-routes/upload.php
@@ -2,13 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-
+Route::post('images', '\App\Http\Controllers\UploadController@UploadToS3');
 
 
 // For Login Users
 Route::group(['middleware' => 'auth:api'], function(){
 
-    Route::post('upload', '\App\Http\Controllers\UploadController@upload');
+    Route::post('upload', '\App\Http\Controllers\UploadController@uploadToLocal');
 
 });
 
