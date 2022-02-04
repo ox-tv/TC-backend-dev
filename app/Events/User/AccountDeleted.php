c44872b01d3c499a4543ff28219389eb1a303bb8
Ali <aahelali@gmail.com>
2022-02-04 16:46:09 +0330

    Add event

---
+++ app/Events/User/AccountDeleted.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Events\User;
+
+use App\Models\Message;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class AccountDeleted
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $user;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+}
