9385ac5a18ac59a9943060fb82ee709a16f950c4
Ali <aahelali@gmail.com>
2022-02-07 17:14:19 +0330

    Add upload config

---
+++ config/upload.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'thumbnail_sizes' => [
+        ['w' => 400, 'h' => null],
+        ['w' => 256, 'h' => null],
+        ['w' => 128, 'h' => null],
+        ['w' => 48, 'h' => null],
+    ],
+
+];
