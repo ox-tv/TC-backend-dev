d2a90e6e2f82f2f440109ea529089851fd659d98
Amir <a.mir.yousefi.it@gmail.com>
2022-02-25 14:46:37 +0330

    Add has_membership_history to profile

--- app/Http/Resources/UserItem.php
+++ app/Http/Resources/UserItem.php
@@ -57,6 +57,7 @@ class UserItem extends JsonResource
             'hero_member_at' => $this->hero_member_at,
             'hero_due_at' => $this->hero_due_at,
             'is_hero' => $this->is_hero,
+            'has_membership_history' => $this->has_membership_history,
             'is_mute' => $this->is_mute,
             'muted_until' => $this->muted_until,
             'liked_videos_count' => $this->likedVideos()->count(),
