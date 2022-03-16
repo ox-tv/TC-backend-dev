3138fbd4f2bf35da144aa246538bceda8ada7824
Ali <aahelali@gmail.com>
2022-03-16 12:43:06 +0330

    Add layers keys to VideoMeta model and refactor returned value

--- app/Models/VideoMeta.php
+++ app/Models/VideoMeta.php
@@ -10,9 +10,25 @@ class VideoMeta extends Model
     protected $fillable = ['key', 'value'];
 
     protected $table = 'video_meta';
-    public $timestamps = false;
 
+    const VIDEO_LAYERS = 'layers';
+    const VIDEO_LAYERS_DRAFT = 'layers_draft';
+
+    const KEYS_WITH_JSON_VALUE = [
+        self::VIDEO_LAYERS,
+        self::VIDEO_LAYERS_DRAFT,
+    ];
+
+    // Relations
     public function video(){
         return $this->belongsTo('App\Models\Video');
+    }
+
+    // Attributes
+    public function getValueAttribute($value){
+        if (in_array($this->key, self::KEYS_WITH_JSON_VALUE)){
+            return json_decode($value, true);
+        }
+        return $value;
     }
 }
