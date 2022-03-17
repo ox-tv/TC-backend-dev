f172c08b6632122fb1c4dc6c55b59a35058b3874
Ali <aahelali@gmail.com>
2022-03-17 11:56:13 +0330

    Just test route

--- routes/partial-routes/general.php
+++ routes/partial-routes/general.php
@@ -8,7 +8,12 @@ Route::get('home', '\App\Http\Controllers\GeneralController@home');
 // search
 Route::get('search/{keyword}', '\App\Http\Controllers\SearchController@index');
 
-
+Route::get('test', function (){
+    $playlist = \App\Models\Tag::with([])
+        ->first()
+        ->append(['favorited_by_users_count', 'videos_count']);
+    return  \App\Http\Resources\Tag\TagResource::make($playlist);
+});
 
 // For Publishers
 Route::group([
