78436d498fe3fac1d3e50c5fd4477c0ff521181c
Ali <aahelali@gmail.com>
2022-03-28 18:09:28 +0430

    Add LanguageResource

---
+++ app/Http/Resources/Language/LanguageResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Language;
+
+use App\Http\Resources\PaymentMethod\PaymentMethodItem;
+use App\Models\Transaction;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LanguageResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'code' => $this->code,
+            'display_name' => $this->display_name,
+        ];
+    }
+}
