f8e2fa0d5ece451414b315e30a7f04c9e25ff662
Ali <aahelali@gmail.com>
2022-03-29 14:16:16 +0430

    use new chapter resource

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -2,13 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\Chapter\ChapterStore;
-use App\Http\Requests\Chapter\ChapterUpdate;
-use App\Http\Resources\Chapter\ChapterItem;
 use App\Http\Resources\Language\LanguageItem;
-use App\Models\Chapter;
 use App\Models\Language;
-use App\Models\Video;
 use Illuminate\Http\Request;
 
 class LanguageController extends Controller
