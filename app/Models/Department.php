ae097d7a64ccc7b5fdeba97c9ae35cf9526fff1b
Ali <aahelali@gmail.com>
2022-04-01 12:07:22 +0430

    Define Scopes, Add some eloquent scopes and attributes on Department model

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -10,4 +10,40 @@ class Department extends Model
     use HasFactory;
 
     protected $fillable = ['name'];
+
+    // Scope field values
+    const SCOPE_GLOBAL = 1;
+    const SCOPE_PUBLISHER = 2;
+    const SCOPE_USER = 3;
+
+    const SCOPE_TEXT = [
+        self::SCOPE_GLOBAL => 'global',
+        self::SCOPE_PUBLISHER => 'publisher',
+        self::SCOPE_USER => 'user',
+    ];
+
+
+    // Scopes
+
+    public function scopeScopePublisherOrGlobal($query){
+        return $query->where(function ($query) {
+            $query->where('scope', self::SCOPE_PUBLISHER)
+                ->orWhere('scope', self::SCOPE_GLOBAL);
+        });
+    }
+
+    public function scopeScopeUserOrGlobal($query){
+        return $query->where(function ($query) {
+            $query->where('scope', self::SCOPE_USER)
+                ->orWhere('scope', self::SCOPE_GLOBAL);
+        });
+    }
+
+
+    // Attributes
+
+    public function getScopeTextAttribute()
+    {
+        return self::SCOPE_TEXT[$this->scope]?? $this->scope;
+    }
 }
