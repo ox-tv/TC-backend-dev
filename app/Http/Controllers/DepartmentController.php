597e7a7b20cb8f32e0f5cc74f6842776b0da2965
Ali <aahelali@gmail.com>
2022-04-01 12:08:49 +0430

    check scope on index method

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -3,13 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\Department\DepartmentCollection;
+use App\Http\Resources\Department\DepartmentItem;
 use App\Models\Department;
 use Illuminate\Http\Request;
 
 class DepartmentController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return DepartmentCollection::make(Department::all());
+        $query = Department::query();
+
+        if($request->is('api/departments')){
+            $query->scopeUserOrGlobal();
+        }elseif ($request->is('api/publisher/departments')){
+            $query->scopePublisherOrGlobal();
+        }
+
+        return DepartmentItem::collection($query->get());
     }
 }
