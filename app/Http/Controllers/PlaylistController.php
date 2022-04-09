b0504b0a5e800485a36f851d9240b5a672d67de6
Ali <aahelali@gmail.com>
2022-04-09 14:58:59 +0430

    Use new PlaylistResource on PlaylistController

--- app/Http/Controllers/PlaylistController.php
+++ app/Http/Controllers/PlaylistController.php
@@ -4,24 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\PlaylistStore;
 use App\Http\Requests\PlaylistUpdate;
-use App\Http\Resources\PlaylistCollection;
-use App\Http\Resources\PlaylistItem;
+use App\Http\Resources\Playlist\PlaylistResource;
 use App\Models\Channel;
 use App\Models\Playlist;
-use App\Models\User;
 use App\Models\Video;
-use Exception;
-use http\Env\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class PlaylistController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return PlaylistCollection
-     */
     public function index(Request $request, $channelIdOrSlug = null)
     {
         if($channelIdOrSlug){
@@ -39,15 +30,11 @@ class PlaylistController extends Controller
 
         $playlists = $query->get();
 
-        return new PlaylistCollection($playlists);
+        $playlists->append(['total_videos_count', 'published_videos_count']);
+
+        return new PlaylistResource($playlists);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param PlaylistStore $request
-     * @return PlaylistItem
-     */
     public function store(PlaylistStore $request)
     {
         $playlist = new Playlist();
@@ -63,28 +50,16 @@ class PlaylistController extends Controller
 
         $playlist->save();
 
-        return new PlaylistItem($playlist);
+        return new PlaylistResource($playlist);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param Playlist $playlist
-     * @return PlaylistItem
-     */
     public function show($idOrHash)
     {
         $playlist = Playlist::public()->where('id', $idOrHash)->orWhere('url_hash', $idOrHash)->firstOrFail();
-        return new PlaylistItem($playlist);
+        $playlist->append(['total_videos_count', 'published_videos_count']);
+        return new PlaylistResource($playlist);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param PlaylistUpdate $request
-     * @param Playlist $playlist
-     * @return PlaylistItem
-     */
     public function update(PlaylistUpdate $request, Playlist $playlist)
     {
         if (!request()->is('api/admin/*') && $playlist->user_id != auth('api')->id()){
@@ -97,16 +72,9 @@ class PlaylistController extends Controller
         $playlist->owner()->associate(Auth::user());
         $playlist->save();
 
-        return new PlaylistItem($playlist);
+        return new PlaylistResource($playlist);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param Playlist $playlist
-     * @return void
-     * @throws Exception
-     */
     public function destroy(Playlist $playlist)
     {
         if (!request()->is('api/admin/*') && $playlist->user_id != auth('api')->id()){
@@ -119,12 +87,8 @@ class PlaylistController extends Controller
         return response()->json(['message' => 'ok']);
     }
 
-    /**
-     * @param Playlist $playlist
-     * @param Video $video
-     */
-    public function add(Playlist $playlist, Video $video){
-
+    public function add(Playlist $playlist, Video $video)
+    {
         if($playlist->user_id != Auth::user()->id){
             abort(404, 'general.not_found');
         }
@@ -136,12 +100,8 @@ class PlaylistController extends Controller
         ]);
     }
 
-    /**
-     * @param Playlist $playlist
-     * @param Video $video
-     */
-    public function remove(Playlist $playlist, Video $video){
-
+    public function remove(Playlist $playlist, Video $video)
+    {
         if($playlist->owner->id != Auth::user()->id){
             return response()->json([
                 'message' => 'general.not_found'
@@ -155,7 +115,8 @@ class PlaylistController extends Controller
         ]);
     }
 
-    public function bulkAdd(Request $request){
+    public function bulkAdd(Request $request)
+    {
         $playlists = Playlist::where('user_id', auth('api')->id())
             ->whereIn('id', $request->get('playlists',[]))
             ->get();
@@ -168,10 +129,10 @@ class PlaylistController extends Controller
         return response()->json([
             'message' => 'general.successful'
         ]);
-
     }
 
-    public function bulkRemove(Request $request){
+    public function bulkRemove(Request $request)
+    {
         $playlists = Playlist::whereIn('id', $request->get('playlists',[]))->get();
         $videos = Video::whereIn('id', $request->get('videos', []))->get();
 
@@ -182,6 +143,5 @@ class PlaylistController extends Controller
         return response()->json([
             'message' => 'general.successful'
         ]);
-
     }
 }
