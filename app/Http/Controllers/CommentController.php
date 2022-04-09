e66275e4eda2375fde3d8064d58017f3eb2b6f90
Ali <aahelali@gmail.com>
2022-04-09 13:34:51 +0430

    Use new CommentResource on show method

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -85,7 +85,28 @@ class CommentController extends Controller
      */
     public function show(Comment $comment)
     {
-        return new CommentItem($comment);
+        $comment->load([
+            'video',
+            'replies',
+            'user',
+        ])->append([
+            'is_liked',
+            'is_disliked',
+            'likes_count',
+            'dislikes_count',
+            'replies_count',
+        ]);
+
+        $comment->replies->load([
+            'user',
+        ])->append([
+            'is_liked',
+            'is_disliked',
+            'likes_count',
+            'dislikes_count',
+        ]);
+
+        return CommentResource::make($comment);
     }
 
     /**
