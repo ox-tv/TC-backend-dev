23ca27287d9e9942b5f5f319cf3383040beda56b
Amir <a.mir.yousefi.it@gmail.com>
2022-04-21 11:25:07 +0430

    Update text for fillTagsForCustomFeed notification

--- app/Listeners/SendNotificationOnUserVerified.php
+++ app/Listeners/SendNotificationOnUserVerified.php
@@ -20,8 +20,8 @@ class SendNotificationOnUserVerified
     public function handle(UserVerified $event)
     {
         $user = $event->user;
-        $customFeedTagsText = "Hello there! <br/>
-We hope you enjoy Today's Crypto and have had time to click around a bit on the platform. To get the most out of your experience possible, we recommend setting up your custom content feed. You will find this under your profile and \"My Custom Feed.\" There you can add tags for everything you think is relevant to you. You will then see this related content at the top of your home page when you are logged in. You can also choose to get content based on your favorite cryptocurrencies that you have selected under the market page. Stay tight!";
+        $customFeedTagsText = "Hey! <br/>
+Improve your experience by setting up your custom feed. By doing so, you will create a more relevant content feed based on your favorite cryptos. *Set up your custom feed now.";
 
         TCNotification::send(collect([$user]), new FillCustomFeedTags(
             Notification::SCOPE_TEXT[Notification::SCOPE_GLOBAL],
