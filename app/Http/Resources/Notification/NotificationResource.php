86b1502d14645ae2861582e3b19d0b9b1a9149c9
Ali <aahelali@gmail.com>
2022-04-29 17:18:05 +0430

    Add new NotificationResource

---
+++ app/Http/Resources/Notification/NotificationResource.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Resources\Notification;
+
+use App\Http\Resources\Category\CategoryMinimalItem;
+use App\Http\Resources\Category\CategoryResource;
+use App\Http\Resources\Channel\ChannelMinimalItem;
+use App\Http\Resources\Channel\ChannelResource;
+use App\Http\Resources\Comment\CommentItem;
+use App\Http\Resources\Comment\CommentResource;
+use App\Http\Resources\Department\DepartmentItem;
+use App\Http\Resources\Report\ReportItem;
+use App\Http\Resources\Report\ReportMinimalItem;
+use App\Http\Resources\User\UserMinimalItem;
+use App\Http\Resources\User\UserResource;
+use App\Http\Resources\Video\VideoMinimalItem;
+use App\Http\Resources\Video\VideoResource;
+use App\Models\Category;
+use App\Models\Channel;
+use App\Models\Comment;
+use App\Models\Message;
+use App\Models\MessageUser;
+use App\Models\Notification;
+use App\Models\Report;
+use App\Models\User;
+use App\Models\Video;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class NotificationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            // Main attributes
+            'id' => $this->id,
+            'type' => $this->type,
+            'payload' => $this->payload,
+            'created_at' => $this->created_at,
+            'read_at' => $this->read_at,
+
+            // Custom attributes without query
+            'scope' => $this->scope_text,
+            'user_group' => $this->user_group_text,
+
+            // Custom attributes with query
+
+            // Relations
+            'from' => UserResource::make($this->whenLoaded('from')),
+            'entity' => $this->getEntity(),
+            'to' => $this->when(
+                $this->resource->user_group == Notification::USER_GROUP_CUSTOM,
+                UserResource::collection($this->whenLoaded('users'))
+            ),
+        ];
+    }
+
+    private function getEntity()
+    {
+        $resources = [
+            User::class => UserResource::class,
+            Video::class => VideoResource::class,
+            Channel::class => ChannelResource::class,
+            Category::class => CategoryResource::class,
+            Comment::class => CommentResource::class,
+            Report::class => ReportItem::class,
+        ];
+
+        return !empty($resources[$this->resource->entity_type])?
+            $resources[$this->resource->entity_type]::make($this->whenLoaded('entity'))
+            : $this->whenLoaded('entity');
+    }
+}
