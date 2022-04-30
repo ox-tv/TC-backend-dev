c121b3b7f7855beaf3f36f0638a14972ae3dc8a9
Ali <aahelali@gmail.com>
2022-04-30 16:43:22 +0430

    Add new store method on notification repository

--- app/Repository/Eloquent/NotificationRepository.php
+++ app/Repository/Eloquent/NotificationRepository.php
@@ -30,4 +30,24 @@ class NotificationRepository implements NotificationRepositoryInterface
 
         return true;
     }
+
+    public function store2($users, $data)
+    {
+        $notification = new Notification();
+        $notification->type = $data['type'];
+        $notification->scope = array_flip(Notification::SCOPE_TEXT)[$data['scope']];
+        $notification->payload = $data['payload'];
+        $notification->user_group = array_flip(Notification::USER_GROUP_TEXT)[$data['user_group']];
+        $notification->sender_id = $data['sender_id'];
+        $notification->entity_type = $data['entity_type'];
+        $notification->entity_id = $data['entity_id'];
+        $notification->published_at = $data['published_at'];
+
+        DB::transaction(function () use ($notification, $users){
+            $notification->save();
+            $notification->users()->attach($users->pluck('id')->toArray());
+        });
+
+        return $notification;
+    }
 }
--- app/Repository/NotificationRepositoryInterface.php
+++ app/Repository/NotificationRepositoryInterface.php
@@ -10,4 +10,5 @@ use App\Models\User;
 interface NotificationRepositoryInterface
 {
     public function store($users, $type, $scope, $userGroup, $payload = null, $entityType = null, $entityId = null, $from = null);
+    public function store2($users, $data);
 }
