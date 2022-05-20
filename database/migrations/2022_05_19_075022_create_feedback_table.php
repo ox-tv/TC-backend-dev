0db9cc5fa2ab32fd5e91a6b779dfd8f322288f6e
Ali <aahelali@gmail.com>
2022-05-20 15:31:59 +0430

    Add feedbacks migration

--- database/migrations/2022_05_19_075022_create_feedback_table.php
+++ database/migrations/2022_05_19_075022_create_feedback_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFeedbacksTable extends Migration
+class CreateFeedbackTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateFeedbacksTable extends Migration
      */
     public function up()
     {
-        Schema::create('feedbacks', function (Blueprint $table) {
+        Schema::create('feedback', function (Blueprint $table) {
             $table->id();
             $table->string('location');
             $table->unsignedTinyInteger('type');
-            $table->smallInteger('value');
-            $table->text('text');
+            $table->smallInteger('value')->nullable();
+            $table->text('text')->nullable();
 
             $table->unsignedBigInteger('user_id')->nullable();
             $table->foreign('user_id')
@@ -36,6 +36,6 @@ class CreateFeedbacksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('feedbacks');
+        Schema::dropIfExists('feedback');
     }
 }
