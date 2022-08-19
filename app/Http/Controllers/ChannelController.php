894bbc464751121e207db50f80f63eb69ae7295f
Ali <aahelali@gmail.com>
2022-08-19 12:14:37 +0430

    Just Enable if

--- app/Http/Controllers/ChannelController.php
+++ app/Http/Controllers/ChannelController.php
@@ -166,7 +166,7 @@ class ChannelController extends Controller
 
     public function update(ChannelUpdate $request, Channel $channel)
     {
-        if(0 && !$request->is('api/admin/channels/*')){
+        if(!$request->is('api/admin/channels/*')){
             $user = auth('api')->user();
 
             $_2fa = $user->_2fa;
