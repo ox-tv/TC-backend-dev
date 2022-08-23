04dcef85f51f0f3bf8e542e99d1c5a2f436a767d
Ali <aahelali@gmail.com>
2022-08-23 13:47:11 +0430

    Add relations, scopes and status on model

--- app/Models/Content.php
+++ app/Models/Content.php
@@ -10,16 +10,49 @@ class Content extends Model
 {
     protected $casts = ['content' => 'array'];
 
-    public function scopeIdOrPage($query, $idOrPage)
+    const STATUS_DRAFT = 1;
+    const STATUS_PUBLISHED = 2;
+
+    const STATUS_TEXT = [
+        self::STATUS_DRAFT => 'draft',
+        self::STATUS_PUBLISHED => 'published',
+    ];
+
+    public function scopeStatus($query, $status)
     {
-        $query->where(function ($q) use ($idOrPage){
-            $q->when(is_numeric($idOrPage), function ($q) use ($idOrPage){
-                $q->where('id', $idOrPage);
-            })->when(!is_numeric($idOrPage), function ($q) use ($idOrPage){
-                $q->where('page', $idOrPage);
+        $status = is_numeric($status)? $status : array_flip(self::STATUS_TEXT)[$status];
+
+        $query->where('status', $status);
+
+        return $query;
+    }
+
+    public function scopeIdOrKey($query, $idOrKey)
+    {
+        $query->where(function ($q) use ($idOrKey){
+            $q->when(is_numeric($idOrKey), function ($q) use ($idOrKey){
+                $q->where('id', $idOrKey);
+            })->when(!is_numeric($idOrKey), function ($q) use ($idOrKey){
+                $q->where('key', $idOrKey);
             });
         });
 
         return $query;
+    }
+
+    // Relations
+    public function lastModifiedBy(){
+        return $this->belongsTo('App\Models\User', 'last_modified_by')->withTrashed();
+    }
+
+    // Attributes
+    public function getStatusTextAttribute()
+    {
+        return self::STATUS_TEXT[$this->status]?? $this->status;
+    }
+
+    public function setStatusAttribute($value)
+    {
+        $this->attributes['status'] = (is_numeric($value))? $value : array_flip(self::STATUS_TEXT)[$value];
     }
 }
