7e9e0d49aa200e8ddc8c1c63524794cc4ece96d0
Ali <aahelali@gmail.com>
2022-08-24 12:13:18 +0430

    Prevent send mention notifications to publishers

--- app/Listeners/Comments/SendNotificationOnCommentCreated.php
+++ app/Listeners/Comments/SendNotificationOnCommentCreated.php
@@ -2,10 +2,12 @@
 
 namespace App\Listeners\Comments;
 
+use Amir\Permission\Models\Role;
 use App\Events\Comments\CommentCreated;
 use App\Events\VideoViewed;
 use App\Http\Resources\Comment\CommentResource;
 use App\Models\Notification;
+use App\Models\User;
 use App\TCNotification\GeneralNotification;
 use TCNotification;
 
@@ -19,18 +21,22 @@ class SendNotificationOnCommentCreated
      */
     public function handle(CommentCreated $event)
     {
+        $publisherRoleId = Role::firstOrCreate(['name' => User::PUBLISHER_ROLE])->id;
+
         $comment = $event->comment;
-        $mentions = $comment->mentions()->get();
+        $mentions = $comment->mentions()->where('role_id', '!=', $publisherRoleId)->get();
 
         $comment->load('user');
 
-        TCNotification::Send($mentions, new GeneralNotification(
-            Notification::TYPE_MENTIONED_ON_COMMENT,
-            Notification::SCOPE_TEXT[Notification::SCOPE_GLOBAL],
-            [
-                'comment' => CommentResource::make($comment),
-            ]
-        ));
+        if ($mentions->isNotEmpty()){
+            TCNotification::Send($mentions, new GeneralNotification(
+                Notification::TYPE_MENTIONED_ON_COMMENT,
+                Notification::SCOPE_TEXT[Notification::SCOPE_GLOBAL],
+                [
+                    'comment' => CommentResource::make($comment),
+                ]
+            ));
+        }
 
         return true;
     }
