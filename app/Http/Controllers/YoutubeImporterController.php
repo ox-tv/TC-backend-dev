4055bfef9e7135da232de6c7d316b17032002c95
Ali <aahelali@gmail.com>
2022-08-27 13:02:33 +0430

    Create new controller for youtube importer requests and add 3 methods

---
+++ app/Http/Controllers/YoutubeImporterController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\Channels\ChannelImportRequestAccepted;
+use App\Events\Channels\ChannelImportRequestCompleted;
+use App\Http\Requests\ChannelImportRequest;
+use App\Http\Resources\Channel\ImportRequestResource;
+use App\Models\Channel;
+use Illuminate\Http\Request;
+
+class YoutubeImporterController extends Controller
+{
+    public function index(Request $request)
+    {
+        $channels = Channel::whereIn('import_request_status', [Channel::IMPORT_STATUS_REQUESTED, Channel::IMPORT_STATUS_SYNC])->get();
+
+        return ImportRequestResource::collection($channels);
+    }
+
+
+    public function importRequest(ChannelImportRequest $request, Channel $channel)
+    {
+        if (in_array($channel->import_request_status, [Channel::IMPORT_STATUS_COMPLETED, Channel::IMPORT_STATUS_SYNC])){
+            return response()->json([
+                'message' => __('channel.import_accept.bad_request'),
+            ], 400);
+        }
+
+        $channel->import_request_status = Channel::IMPORT_STATUS_REQUESTED;
+        $channel->youtube_channel_id = $request->get("youtube_channel_id");
+        $channel->save();
+
+        event(new ChannelImportRequestAccepted($channel));
+
+        return response()->json([
+            'message' => __('channel.messages.import_request_submitted'),
+        ]);
+    }
+
+    public function importCompleted(Channel $channel)
+    {
+        if ($channel->import_request_status == Channel::IMPORT_STATUS_REQUESTED){
+
+            $channel->import_request_status = Channel::IMPORT_STATUS_COMPLETED;
+            $channel->save();
+
+            event(new ChannelImportRequestCompleted($channel));
+
+            return response()->json([
+                'message' => __('channel.messages.import_completed'),
+            ]);
+        }
+
+        return response()->json([
+            'message' => __('channel.import_completed.bad_request'),
+        ], 400);
+    }
+
+}
