4fa0aafbfce6bce7e691a53292c5702c9781b335
Ali <aahelali@gmail.com>
2022-11-16 15:36:03 +0330

    set default per page to 50 on listing

--- app/Http/Controllers/CryptoCurrencyController.php
+++ app/Http/Controllers/CryptoCurrencyController.php
@@ -38,7 +38,7 @@ class CryptoCurrencyController extends Controller
         if ($request->get('per_page') == -1){
             $data = $query->get();
         }else{
-            $data = $query->paginate();
+            $data = $query->paginate($request->get('per_page', 50));
         }
 
         $data->append(['is_favorite']);
