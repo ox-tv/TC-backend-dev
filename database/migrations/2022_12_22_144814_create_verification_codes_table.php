24488b2c4ca3c01d96fc22db2cb5317bb8628af0
Ali <aahelali@gmail.com>
2022-12-26 17:26:13 +0330

    Add verification_codes migration

---
+++ database/migrations/2022_12_22_144814_create_verification_codes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVerificationCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('verification_codes', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+
+            $table->string('code');
+
+            $table->timestamps();
+            $table->timestamp('expired_at');
+            $table->timestamp('verified_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('verification_codes');
+    }
+}
