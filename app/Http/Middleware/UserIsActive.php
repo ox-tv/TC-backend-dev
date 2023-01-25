66a08a1af6fc52a90531e85a8137539df8699d63
Ali <aahelali@gmail.com>
2023-01-25 16:37:08 +0330

    Add middleware to update last_actived_at field

---
+++ app/Http/Middleware/UserIsActive.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+
+class UserIsActive
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (($user = auth('api')->user()) && !Cache::has('user_'.$user->id.'_last_actived_at_stored')){
+            $user->last_actived_at = Carbon::now();
+            $user->save();
+            Cache::put('user_'.$user->id.'_last_actived_at_stored', true, 600);
+        }
+
+        return $next($request);
+    }
+}
