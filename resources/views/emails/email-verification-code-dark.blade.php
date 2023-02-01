5e6463adaed4991358bcc2f6921e7c8471ad9034
Ali <aahelali@gmail.com>
2023-02-01 19:14:28 +0330

    HotFix emails title

--- resources/views/emails/email-verification-code-dark.blade.php
+++ resources/views/emails/email-verification-code-dark.blade.php
@@ -13,7 +13,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Publisher Request Rejected</title>
+    <title>Verification Code</title>
 
     <style type="text/css">
         p{
