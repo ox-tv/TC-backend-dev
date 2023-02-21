59fe284eac1e5c62814c89a33c5f393d06797ce4
Ali <aahelali@gmail.com>
2023-02-21 17:25:14 +0330

    Add migration to create companies table

---
+++ database/migrations/2023_02_21_104314_create_companies_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id()->startingValue(10020);
+
+            $table->string('name');
+            $table->text('avatar_url')->nullable();
+
+            $table->string('vat_number')->nullable();
+            $table->unsignedSmallInteger('vat_rate')->nullable();
+
+            $table->string('street_address')->nullable();
+            $table->string('street_no')->nullable();
+            $table->string('postal_code')->nullable();
+            $table->string('city')->nullable();
+            $table->string('country')->nullable();
+
+            $table->string('contact_person_name')->nullable();
+            $table->string('contact_person_email')->nullable();
+            $table->string('contact_person_phone')->nullable();
+
+            $table->string('invocing_questions_email')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
