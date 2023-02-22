0cb8a6ea363fba9048b75a8efeaf1e144927e025
Ali <aahelali@gmail.com>
2023-02-22 18:14:34 +0330

    Add ads_pricing model

---
+++ app/Models/AdPricing.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class AdPricing extends Model
+{
+    protected $table = 'ads_pricings';
+
+    protected $fillable = ['date', 'tier', 'price'];
+
+    public $timestamps = false;
+
+
+    // Relations
+
+
+    // Attributes
+
+}
