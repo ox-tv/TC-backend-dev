f8eaba0e34374696187d21184c951f3e4f8d9d35
Ali <aahelali@gmail.com>
2023-02-28 10:54:27 +0330

    Add resources

---
+++ app/Http/Resources/Ad/AdSlotResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\Ad;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AdSlotResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            // Main attributes
+            'id' => $this->id,
+            "date" => $this->date,
+            'tier' => $this->tier,
+            'price' => $this->price,
+            'quantity' => $this->quantity,
+
+            // Relations
+            'campaign' => AdCampaignResource::make($this->whenLoaded('campaign')),
+        ];
+    }
+}
