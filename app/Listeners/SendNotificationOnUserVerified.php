e9d1b190135a772a890d601c49b378870c737a51
Ali <aahelali@gmail.com>
2023-03-06 13:16:23 +0330

    HotFix add subject to customFeedNotification

--- app/Listeners/SendNotificationOnUserVerified.php
+++ app/Listeners/SendNotificationOnUserVerified.php
@@ -25,13 +25,12 @@ class SendNotificationOnUserVerified
         $user = $event->user;
 
         // CustomFeed Notification
-        $customFeedTagsText = "Hey!
-        Improve your experience by setting up your custom feed. By doing so, you will create a more relevant content feed based on your favorite cryptos. Set up your custom feed now.";
+        $customFeedTagsText = "Improve your experience by setting up your custom feed. By doing so, you will create a more relevant content feed based on your favorite cryptos. Set up your custom feed now.";
 
         TCNotification::Send(collect([$user]), new GeneralNotification(
             Notification::TYPE_FILL_CUSTOM_FEED_TAGS,
             Notification::SCOPE_TEXT[Notification::SCOPE_GLOBAL],
-            ['message' => $customFeedTagsText]
+            ['message' => $customFeedTagsText, 'subject' => 'Hey!']
         ));
 
 
