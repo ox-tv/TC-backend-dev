485e4f9465f83fe1d9b4f52307a3640e20699533
Ali <aahelali@gmail.com>
2023-05-12 14:28:02 +0330

    Remove testing codes

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $client = new \App\Libraries\TCPolygonClient();
-    $res = $client->ClaimToken('0xaC2F4DD11455B65Bf4e0f42eD6eC45ebDec1840E', strval(50));
-    dd($res);
     return view('welcome');
 });
 
