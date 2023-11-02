07c61f5e10c7ac8c5e0edcc851111e9ee62bdc2e
Ali <aahelali@gmail.com>
2023-11-02 14:39:56 +0330

    Some tests

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $now = \App\Models\CryptoCurrencyPrice::fromDateTime(\Carbon\Carbon::now());
+    $data = [
+        ['Crypto_currency_id' => 1, 'price' => 110, 'last_updated' => $now],
+        ['Crypto_currency_id' => 5, 'price' => 34152, 'last_updated' => $now],
+        ['Crypto_currency_id' => 3, 'price' => 0.00753, 'last_updated' => $now],
+    ];
+    \App\Models\CryptoCurrencyPrice::insert($data);
     return view('welcome');
 });
 
