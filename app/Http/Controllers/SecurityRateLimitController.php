80e519d4baeb1313963bda24e304218878231f7c
Ali <aahelali@gmail.com>
2023-11-18 20:15:06 +0330

    Enhance SecurityRateLimitController

--- app/Http/Controllers/SecurityRateLimitController.php
+++ app/Http/Controllers/SecurityRateLimitController.php
@@ -8,7 +8,9 @@ use App\Http\Requests\Tag\TagUpdate;
 use App\Http\Resources\Tag\TagResource;
 use App\Models\SecurityRateLimit;
 use App\Models\Tag;
+use App\Models\TokenPoint;
 use App\Repository\Eloquent\TagRepository;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 
@@ -34,7 +36,26 @@ class SecurityRateLimitController extends Controller
             ]);
         });
 
-        return response()->json($data);
+
+        $userIds = SecurityRateLimit::raw(function($collection){
+            return $collection->aggregate([
+                ['$group' => [
+                    '_id' => '$user_id',
+                    'count' => [
+                        '$sum' => 1
+                    ],
+                ]],
+                ['$match' => [
+                    'count' => ['$lte'=> 100],
+                ]],
+                ['$sort' => ['count' => -1]],
+            ]);
+        })->pluck('_id')->toArray();
+
+        $amount = TokenPoint::whereIn('user_id', $userIds)->whereNull('claimable_at')->where('activate_at', '>=', Carbon::now()->subDay()->startOfDay())->sum('amount');
+
+
+        return response()->json(['group_by_user' => $data, 'amount_of_users_below_100_requests' => $amount]);
     }
 
 }
