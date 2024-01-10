6d136485af27a4d187663d0063afdb0017613734
ali <aahelali@gmail.com>
2024-01-10 13:19:34 +0330

    Add some config to user config

--- config/user.php
+++ config/user.php
@@ -12,4 +12,22 @@ return [
         'per_referrer_as_non_hero' => env('USER_POINT_PER_REFERRER_AS_NON_HERO', 1000),
     ],
 
+    'nft_contract_addresses' => [
+        'white' => env('NFT_WHITE_CONTRACT_ADDRESS'),
+        'black' => env('NFT_BLACK_CONTRACT_ADDRESS'),
+    ],
+
+    'max_token_for_watching_video_per_day' => [
+        'white_1' => 60,
+        'white_1,5' => 90,
+        'white_2' => 120,
+        'white_2,5' => 150,
+        'white_3' => 180,
+        'black_1' => 300,
+        'black_1,5' => 450,
+        'black_2' => 600,
+        'black_2,5' => 750,
+        'black_3' => 900,
+    ],
+
 ];
