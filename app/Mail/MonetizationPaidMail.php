200d0fbfc4e0cfc6503bfeec15955de2c01302cf
ali <aahelali@gmail.com>
2024-04-04 16:12:48 +0330

    Add MonetizationPaidMail

---
+++ app/Mail/MonetizationPaidMail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class MonetizationPaidMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $amount;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($amount)
+    {
+        $this->amount = $amount;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('monetize@todayscrypto.com')->subject('You’ve got paid!')->view('emails.monetization-paid-dark');
+    }
+}
