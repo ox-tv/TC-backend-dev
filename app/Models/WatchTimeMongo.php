2fbd5f6ec7f1a3cbd69c16b0918b86ce08c2d56c
ali <aahelali@gmail.com>
2024-06-03 14:23:43 +0330

    Add whereDateScope to WatchTimeMongo

--- app/Models/WatchTimeMongo.php
+++ app/Models/WatchTimeMongo.php
@@ -11,6 +11,7 @@ class WatchTimeMongo extends Model
 {
     protected $connection = 'mongodb';
     protected $collection = 'watch_times';
+    protected $fillable = ['video_id', 'user_id', 'start_time', 'end_time'];
 
     protected $casts = [
         //
@@ -19,5 +20,9 @@ class WatchTimeMongo extends Model
     public const AllRowsCachePeriod = 24 * 60 * 60;
     public const LastRowCachePeriod = 60 * 60;
 
-
+    public function scopeWhereDate($query, $column, $carbonDate)
+    {
+        return $query->where($column, '>=', (clone $carbonDate)->startOfDay())
+            ->where($column, '<=', (clone $carbonDate)->endOfDay());
+    }
 }
